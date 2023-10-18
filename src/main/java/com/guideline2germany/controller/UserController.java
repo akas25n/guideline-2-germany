59702cc646e31eac92f8f0c56aa316b1186b461b
akas25n <akas25n@gmail.com>
2023-10-18 12:02:42 +0200

    Exceptions hinzugefügt

--- src/main/java/com/guideline2germany/controller/UserController.java
+++ src/main/java/com/guideline2germany/controller/UserController.java
@@ -1,25 +1,24 @@
 package com.guideline2germany.controller;
 
 import com.guideline2germany.entity.User;
+import com.guideline2germany.exceptions.ResourceNotFoundException;
 import com.guideline2germany.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
 
-    @Autowired
     private UserService userService;
 
-    @RequestMapping("/greeting")
-    public String greeting(){
-        return "Hey there!";
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @RequestMapping("/users")
@@ -30,5 +29,27 @@ public class UserController {
     @RequestMapping(method = RequestMethod.POST, value = ("/users"))
     public void createNewUser(@RequestBody User user){
         userService.createUser(user);
+    }
+
+    @RequestMapping("/users/{id}" )
+    public ResponseEntity<User> findUserById(@PathVariable long id) throws ResourceNotFoundException {
+        User user = userService.findUser(id);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/users/{id}")
+    public ResponseEntity<User> updateUser(@RequestBody User newUser, @PathVariable long id) throws ResourceNotFoundException {
+       User user = userService.updateUserData(newUser, id);
+       return ResponseEntity.ok().body(user);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = ("/users/{id}"))
+    public Map<String, Boolean> deleteCurrentUser(@PathVariable long id) throws ResourceNotFoundException{
+        userService.deleteUser(id);
+
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+
+        return response;
     }
 }
--- src/main/java/com/guideline2germany/service/UserService.java
+++ src/main/java/com/guideline2germany/service/UserService.java
@@ -1,8 +1,8 @@
 package com.guideline2germany.service;
 
 import com.guideline2germany.entity.User;
+import com.guideline2germany.exceptions.ResourceNotFoundException;
 import com.guideline2germany.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,8 +10,11 @@ import java.util.List;
 @Service
 public class UserService {
 
-    @Autowired
     private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public List<User> getAllUsers(){
         return userRepository.findAll();
@@ -25,5 +28,30 @@ public class UserService {
         newUser.setEmailAddress(user.getEmailAddress());
 
         userRepository.save(newUser);
+    }
+
+    public User findUser(Long id) throws ResourceNotFoundException{
+        User user = userRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("User not found for this id :: " + id));
+        return user;
+    }
+
+    public User updateUserData(User newUser, long id) throws ResourceNotFoundException {
+        User currentUser = userRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("User not found for this id :: " + id));
+
+        currentUser.setFirstName(newUser.getFirstName());
+        currentUser.setLastName(newUser.getLastName());
+        currentUser.setMobileNumber(newUser.getMobileNumber());
+        currentUser.setEmailAddress(newUser.getEmailAddress());
+
+        User updatedUser = userRepository.save(currentUser);
+        return updatedUser;
+    }
+
+    public void deleteUser(long id) throws ResourceNotFoundException{
+        User user = userRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("User not found for this id :: " + id));
+        userRepository.delete(user);
     }
 }
